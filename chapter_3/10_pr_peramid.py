9d154597a9e40cf085048a1f5da7addaa9b6ed52
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-24 00:05:24 +0530

    add new files

--- chapter_3/10_pr_peramid.py
+++ chapter_3/10_pr_peramid.py
@@ -1,8 +1,11 @@
 #1. Simple Star Pattern
 for i in range(1, 6): #LEFT SIDE PYRAMID
     print("*" * i)
+#2 Simple reverse peramid
+for i in range(5, 0, -1):
+    print("*"*i)
 
-#2.  Pyramid
+#3.  Pyramid
 for i in range(1,6):
     print(" "*(5-i), end="")
     print("* "*i)
