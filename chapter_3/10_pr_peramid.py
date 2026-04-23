d7993f47dd2f5ad3e1055281f55cf24c4260ae9a
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-23 23:57:53 +0530

    add new files

--- chapter_3/10_pr_peramid.py
+++ chapter_3/10_pr_peramid.py
@@ -9,3 +9,8 @@ for i in range(1,6):
 print("INVERTED PIRAMID STAR")    
 for i in range(5, 0, -1):
     print("*" * i) # PYRAMId STAR
+    
+print("INVERTED PIRAMID STAR")
+for i in range(5, 0, -1):
+    print(" "*(5-i), end="")
+    print("* "*i)
