32e491bb8e85dfd15828f811812914eed38054b0
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-24 13:03:23 +0530

    add new files

--- chapter_3/11_pr_even_odd pyramid.py
+++ chapter_3/11_pr_even_odd pyramid.py
@@ -1,12 +1,26 @@
 
-#6.Even numbers pyramid upto 10
+'''#6.Even numbers pyramid upto 10
 print("\n")
 #method 1
 n=5
 for i in range(n):
     print(" "*(n-i-1)+"*"*(2*i+2))
- 
+    
+print("\n")
 #method 2
 n=5
 for i in range(1, n+1):
     print(" "*(n-i)+"*"*(2*i))
+'''
+#7. Odd numbers pyramid upto 10
+print("method 1","\n") 
+#method 1
+n=5
+for i in range(n):
+    print(" "*(n-i-1)+"*"*(2*i+1))
+    
+print("Method 2","\n")
+#method 2
+n=5
+for i in range(1, n+1):
+    print(" "*(n-i)+"*"*(2*i-1))
