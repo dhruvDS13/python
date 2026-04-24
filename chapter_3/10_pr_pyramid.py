f36e38882b1b7c256f2ecc4ab0ba24cb616460f2
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-24 12:57:08 +0530

    add new files

--- chapter_3/10_pr_pyramid.py
+++ chapter_3/10_pr_pyramid.py
@@ -33,21 +33,10 @@ Method 1 Basic
 n=5
 for i in range(n):
     print(" "*(n-i-1)+"*"*(2*i+1))
-
+    
+#Method 3
 n=5
 for i in range(1,n+1):
     print(" "*(n-i)+"*"*(2*i-1))
     
-'''  
-#6.Even numbers pyramid upto 10
-print("\n")
-#method 1
-n=5
-for i in range(n):
-    print(" "*(n-i-1)+"*"*(2*i+2))
- 
-#method 2
-n=5
-for i in range(1, n+1):
-    print(" "*(n-i)+"*"*(2*i))
-    '''  
+'''
