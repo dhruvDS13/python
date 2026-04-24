eff6767508401208dc9da9bcc177cf7ca76ace53
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-24 12:42:06 +0530

    add new files

--- chapter_3/10_pr_peramid.py
+++ chapter_3/10_pr_peramid.py
@@ -1,4 +1,5 @@
-#1. Simple Star Pattern
+'''
+#1. Left Star Pattern
 for i in range(1, 6): #LEFT SIDE PYRAMID
     print("*" * i)
     
@@ -7,7 +8,7 @@ for i in range(1,6):
     print(" "*(5-i), end="")
     print("*" * i)
 
-#3 Simple reverse pyramid
+#3 left reverse pyramid
 print("\n")
 for i in range(5, 0, -1):   # -1 is used to decrease the value of i by 1 in each iteration
     print("*" * i)
@@ -17,3 +18,24 @@ print("\n")
 for i in range(5,0, -1):
     print(" "*(5-i), end=" ")
     print("*" *i)
+    '''
+
+#5.  Pyramid
+print("\n")
+
+'''Method 1 Basic
+    for i in range(1,6):
+        print(" "*(5-i), end="")
+        print("*" * (2*i-1))
+'''
+'''    #Method 2 
+n=5
+for i in range(n):
+    print(" "*(n-i-1)+"*"*(2*i+1))
+'''
+
+#6.Even numbers pyramid upto 10
+print("\n")
+n=5
+for i in range(n):
+    print(" "*(n-i-1)+"*"*(2*i+2))
