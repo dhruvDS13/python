4a3507b4d266a0761548ab76f798a591f9aded4d
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-24 22:02:16 +0530

    update new files

--- chapter_3/13_pr_print_reverse_star9.py
+++ chapter_3/13_pr_print_reverse_star9.py
@@ -1,3 +1,11 @@
 n=9
-for i in range(n):
-    print()
+space=0
+for i in range(n,0,-1):
+    if i%2!=0:
+        print(" "*space + "*"*(i))
+        space+=1
+
+n=9
+space=0
+for i in range(1,n+1):
+    print(" "*(n-i)+ "*"*(2*i-1))
