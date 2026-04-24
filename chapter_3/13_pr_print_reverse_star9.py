e8fe65e7f73ea2f2e387c032d9c5942029dd7958
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-24 22:23:53 +0530

    update new files

--- chapter_3/13_pr_print_reverse_star9.py
+++ chapter_3/13_pr_print_reverse_star9.py
@@ -1,21 +1,32 @@
+print("P1 % USed","\n")
 n=9
 space=0
 for i in range(n,0,-1):
     if i%2!=0:
         print(" "*space + "*"*(i))
-        if i==True:
-            print(i)
         space+=1
-
+print("P2","\n")
 i=5
 if i% 2!=0:
     print("odd")
 else:
     print("even")
-    
-n=5
-space=0
-for i in range(1,n+1):
+
+print("P3","\n")
+n=5 # ye 9 star tak print karega
+for i in range(1,n+1,-1):
     print(" "*(n-i)+ "*"*(2*i-1))
 
+print("P4 Direct Formula","\n")
+n=5
+for i in range(n,0,-1):
+    print(" "*(n-i)+ "*"*(2*i-1))
 
+print("P5 Using While Loop","\n")
+n=9
+space =0 
+while n>0:
+    print(" "*space + "*"*n)
+    n-=2
+    space+=1
+    
