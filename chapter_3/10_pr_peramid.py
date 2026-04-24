059ab2f51f8b3323619f418cae52d12c30fe9b34
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-24 12:23:38 +0530

    add new files

--- chapter_3/10_pr_peramid.py
+++ chapter_3/10_pr_peramid.py
@@ -11,3 +11,9 @@ for i in range(1,6):
 print("\n")
 for i in range(5, 0, -1):   # -1 is used to decrease the value of i by 1 in each iteration
     print("*" * i)
+
+#4. Reverse right side pyramid
+print("\n") 
+for i in range(5,0, -1):
+    print(" "*(5-i), end=" ")
+    print("*" *i)
