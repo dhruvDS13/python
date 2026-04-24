2dff6e705c08a7566aad31455566c0c83c6fdb89
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-24 22:09:44 +0530

    update new files

--- chapter_3/13_pr_print_reverse_star9.py
+++ chapter_3/13_pr_print_reverse_star9.py
@@ -3,9 +3,19 @@ space=0
 for i in range(n,0,-1):
     if i%2!=0:
         print(" "*space + "*"*(i))
+        if i==True:
+            print(i)
         space+=1
 
-n=9
+i=5
+if i% 2!=0:
+    print("odd")
+else:
+    print("even")
+    
+n=5
 space=0
 for i in range(1,n+1):
     print(" "*(n-i)+ "*"*(2*i-1))
+
+
