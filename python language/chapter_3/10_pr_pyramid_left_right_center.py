fb5483a3e842f9564c108ddfce148379295dd194
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-26 18:24:28 +0530

    update new files

--- python language/chapter_3/10_pr_pyramid_left_right_center.py
+++ python language/chapter_3/10_pr_pyramid_left_right_center.py
@@ -31,21 +31,5 @@ for i in range(5,0, -1):
     print("*" *i)
 '''
 
-#5.  Pyramid
-print("\n" ,"odd pyramind")
 
-#Method 1 Basic
-for i in range(1,6):
-    print(" "*(5-i), end="")
-    print("*" * (2*i-1))
-    
-#Method 2 
-n=5
-for i in range(n):
-    print(" "*(n-i-1)+"*"*(2*i+1))
-    
-#Method 3
-n=5
-for i in range(1,n+1):
-    print(" "*(n-i)+"*"*(2*i-1))
-    
+
