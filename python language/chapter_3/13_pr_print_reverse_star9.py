6a81770e974d9e6d56d73c249698fce8ddd7df47
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-26 18:27:51 +0530

    update new files

--- python language/chapter_3/13_pr_print_reverse_star9.py
+++ python language/chapter_3/13_pr_print_reverse_star9.py
@@ -5,23 +5,26 @@ for i in range(n,0,-1):
     if i%2!=0:
         print(" "*space + "*"*(i))
         space+=1
+# check if n is odd or even
 print("P2","\n")
 i=5
 if i% 2!=0:
     print("odd")
 else:
     print("even")
-
+# Basic method
 print("P3","\n")
 n=5 # ye 9 star tak print karega
 for i in range(1,n+1,-1):
     print(" "*(n-i)+ "*"*(2*i-1))
 
+# Direct formula
 print("P4 Direct Formula","\n")
 n=5
 for i in range(n,0,-1):
     print(" "*(n-i)+ "*"*(2*i-1))
 
+# Using while loop
 print("P5 Using While Loop","\n")
 n=9
 space =0 
@@ -30,6 +33,7 @@ while n>0:
     n-=2
     space+=1
 
+# Using List + Join
 print("P6 Using List + Join","\n")
 n = 5
 
@@ -38,6 +42,7 @@ for i in range(n, 0, -1):
     stars = ["*"] * (2*i - 1)
     print("".join(spaces + stars))
 
+# Using String Center Method
 print("P6 Using String","\n")
 n = 5
 
