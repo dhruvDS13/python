a2f108aab362aff22b3c6b367b5ef537d7746c46
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-26 17:56:52 +0530

    update new files

--- python language/chapter_3/11_pr_even_odd pyramid.py
+++ python language/chapter_3/11_pr_even_odd pyramid.py
@@ -1,4 +1,22 @@
+#5.  Pyramid of odd numbers
+print("\n" ,"odd pyramind")
 
+#Method 1 Basic
+for i in range(1,6):
+    print(" "*(5-i), end="")
+    print("*" * (2*i-1))
+    
+#Method 2 
+n=5
+for i in range(n):
+    print(" "*(n-i-1)+"*"*(2*i+1))
+    
+#Method 3
+n=5
+for i in range(1,n+1):
+    print(" "*(n-i)+"*"*(2*i-1))
+    
+    
 #6.Even numbers pyramid upto 10
 print("\n")
 #method 1
