5c50557093ca981744c703ab9a5423fb6c342c01
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-29 19:20:51 +0530

    update new files

--- python language/chapter_7/05_range_function.py
+++ python language/chapter_7/05_range_function.py
@@ -1,4 +1,4 @@
 # for i in range(0,8,1): # (start element, n-1,gap  b/w element by default tke 1)
     # print(i)
 for i in range(1,51,5):
-    print(i)
+    print(i,end=" ") # end is used to print in same line with space
