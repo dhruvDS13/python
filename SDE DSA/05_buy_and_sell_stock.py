deddf529e0d64d8a1f3275223dfe44893ba03b23
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-29 11:14:14 +0530

    update new files

---
+++ SDE DSA/05_buy_and_sell_stock.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
+
+        return max_profit
+
+
+#  Driver code (for local testing)
+if __name__ == "__main__":
+    prices = [7, 1, 5, 3, 6, 4]
+    
+    sol = Solution()
+    result = sol.maxProfit(prices)
+    
+    print("Maximum Profit:", result)
