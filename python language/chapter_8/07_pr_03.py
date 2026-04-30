205d0aa97f58d1884ee12aa17595cad78e5302de
Dhruv Kumar Singh <dhruvkrsingh332003@gmail.com>
2026-04-30 22:10:50 +0530

    update new files

--- python language/chapter_8/07_pr_03.py
+++ python language/chapter_8/07_pr_03.py
@@ -1,4 +1,4 @@
 print("How", end=" ")
 print("are", end=" ")
 print("you?", end=", ")
-print("dude!", end="")
+print("dude!", end="") # Last line will not print new line, so next print statement will be in same line
